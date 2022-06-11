5d2472d497f9b0036e5a36283bbe169c7731edc9
Mehedi Hassan <mehedi.all.info@gmail.com>
2022-06-11 12:36:36 +0600

    Initial Commit

---
+++ assets/js/main.js
@@ -0,0 +1,100 @@
+// Main Js File
+$(document).ready(function () {
+    'use strict';
+    owlCarousels();
+
+    // Header Search Toggle
+    var $searchWrapper = $('.header-search-wrapper'),
+    	$body = $('body'),
+        $searchToggle = $('.search-toggle');
+
+	$searchToggle.on('click', function (e) {
+		$searchWrapper.toggleClass('show');
+		$(this).toggleClass('active');
+		$searchWrapper.find('input').focus();
+		e.preventDefault();
+	});
+
+	$body.on('click', function (e) {
+		if ( $searchWrapper.hasClass('show') ) {
+			$searchWrapper.removeClass('show');
+			$searchToggle.removeClass('active');
+			$body.removeClass('is-search-active');
+		}
+	});
+	$('.header-search').on('click', function (e) {
+		e.stopPropagation();
+	});
+    
+    // Menu init with superfish plugin
+    if ( $.fn.superfish ) {
+        $('.menu, .menu-vertical').superfish({
+            popUpSelector: 'ul, .megamenu',
+            hoverClass: 'show',
+            delay: 0,
+            speed: 80,
+            speedOut: 80 ,
+            autoArrows: true
+        });
+    }
+
+	// Mobile Menu Toggle - Show & Hide
+    $('.mobile-menu-toggler').on('click', function (e) {
+		$body.toggleClass('mmenu-active');
+		$(this).toggleClass('active');
+		e.preventDefault();
+    });
+
+    $('.mobile-menu-overlay, .mobile-menu-close').on('click', function (e) {
+		$body.removeClass('mmenu-active');
+		$('.menu-toggler').removeClass('active');
+		e.preventDefault();
+    });
+
+	// Add Mobile menu icon arrows to items with children
+    $('.mobile-menu').find('li').each(function () {
+        var $this = $(this);
+
+        if ( $this.find('ul').length ) {
+            $('<span/>', {
+                'class': 'mmenu-btn'
+            }).appendTo($this.children('a'));
+        }
+    });
+
+    // Mobile Menu toggle children menu
+    $('.mmenu-btn').on('click', function (e) {
+        var $parent = $(this).closest('li'),
+            $targetUl = $parent.find('ul').eq(0);
+
+            if ( !$parent.hasClass('open') ) {
+                $targetUl.slideDown(300, function () {
+                    $parent.addClass('open');
+                });
+            } else {
+                $targetUl.slideUp(300, function () {
+                    $parent.removeClass('open');
+                });
+            }
+
+        e.stopPropagation();
+        e.preventDefault();
+    });
+
+    function owlCarousels($wrap, options) {
+        if ( $.fn.owlCarousel ) {
+            if (typeof $wrap == 'undefined') {
+                $wrap = $('body');
+            }
+            if (options) {
+                owlSettings = $.extend({}, owlSettings, options);
+            }
+            // Init all carousel
+            $wrap.find('[data-toggle="owl"]').each(function () {
+                var $this = $(this),
+                    newOwlSettings = $.extend({}, owlSettings, $this.data('owl-options'));
+                $this.owlCarousel(newOwlSettings);
+            });   
+        }
+    }
+});
